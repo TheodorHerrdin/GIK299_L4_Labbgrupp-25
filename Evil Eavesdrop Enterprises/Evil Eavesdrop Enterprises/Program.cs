0693187118c3b3e0450a12ad26b16c1b2f8b681f
Theodor Herrdin <theodor.herrdin@telia.com>
2025-12-19 14:06:03 +0100

    Add Person model with Gender and Hair types; clean up Main
    Introduced Gender enum, Hair struct, and Person class with relevant properties and ToString override.

--- Evil Eavesdrop Enterprises/Evil Eavesdrop Enterprises/Program.cs
+++ Evil Eavesdrop Enterprises/Evil Eavesdrop Enterprises/Program.cs
@@ -1,13 +1,44 @@
-﻿namespace Evil_Eavesdrop_Enterprises
+﻿using System.Security.Cryptography.X509Certificates;
+
+namespace Evil_Eavesdrop_Enterprises
 {
+    //Enum för att representera kön
+    public enum Gender 
+    {
+        Kvinna,
+        Man,
+        Övrigt
+    }
+
+    //Struct för att representera hår
+    public struct Hair 
+    {
+        public string Color;
+        public string Length;
+    }
+
+    //Klass för att representera en person
+    public class Person 
+    {
+        public Gender Gender;
+        public Hair Hair;
+        public string EyeColor;
+        public DateTime Birthday;
+
+        public override string ToString()
+        {
+            return "Född: " + Birthday.ToShortDateString() +
+                ", kön; " + Gender +
+                ", Ögon: " + EyeColor +
+                ", Hår: " + Hair.Color + ", " + Hair.Length;
+        }
+    }
+
     internal class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
-            Console.WriteLine("Evil Eavesdrop Enterprises is operational.");
-            Console.WriteLine("Hej Shaho");
-                Console.WriteLine("hej");
+
         }
     }
 }
