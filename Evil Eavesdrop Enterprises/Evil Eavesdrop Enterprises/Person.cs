e0a1cf734a3b04cd69852c1e09fbe9fc04a886db
Theodor Herrdin <theodor.herrdin@telia.com>
2025-12-20 17:07:26 +0100

    Skrivit om hela programmet manuellt utan att använda git-hub för det krånglade så mycket.

--- Evil Eavesdrop Enterprises/Evil Eavesdrop Enterprises/Person.cs
+++ Evil Eavesdrop Enterprises/Evil Eavesdrop Enterprises/Person.cs
@@ -6,7 +6,103 @@ using System.Threading.Tasks;
 
 namespace Evil_Eavesdrop_Enterprises
 {
+    //Énum för att hantera kön
+    public enum Gender { Kvinna, Man, Övrigt }
+
+    //Struct för att hantera hår
+    public struct Hair
+    {
+        public string Color;
+        public string Length;
+    }
+
+    //Klass för att hantera personen
     public class Person
     {
+        //Egenskaper
+        public Gender Gender { get; set; }
+        public Hair Hair { get; set; }
+        public string EyeColor { get; set; }
+        public DateTime Birthday { get; set; }
+        public static List<Person> PersonList = new List<Person>();
+        //Konstruktor för att initiera alla egenskaper vid skapandet av objektet
+        public Person(Gender gender, Hair hair, string eyeColor, DateTime birthday)
+        {
+            Gender = gender;
+            Hair = hair;
+            EyeColor = eyeColor;
+            Birthday = birthday;
+        }
+        
+        public static void AddPerson()
+        {
+            try
+            {
+                Console.Write("Ögonfärg: ");
+                String eyeColor = Console.ReadLine();
+                Console.Write("Hårfärg: ");
+                String hairColor = Console.ReadLine();
+                Console.Write("Hårlängd: ");
+                String hairLength = Console.ReadLine();
+                Hair hair = new Hair
+                {
+                    Color = hairColor,
+                    Length = hairLength
+                };
+                Console.WriteLine("Välj Kön:");
+                Console.WriteLine("1: Kvinna");
+                Console.WriteLine("2: Man");
+                Console.WriteLine("3: Övrigt");
+                Console.Write("Val: ");
+
+                int GenderChoice = int.Parse(Console.ReadLine());
+                Gender gender = GenderChoice switch
+                {
+                    1 => Gender.Kvinna,
+                    2 => Gender.Man,
+                    3 => Gender.Övrigt,
+                    _ => throw new Exception("Ogiltigt val för kön.")
+                };
+
+                Console.Write("Födelseår ");
+                int Year = int.Parse(Console.ReadLine());
+
+                Console.Write("Födelsemånad ");
+                int Month = int.Parse(Console.ReadLine());
+
+                Console.Write("Födelsdag ");
+                int Day = int.Parse(Console.ReadLine());
+
+                DateTime Birthday = new DateTime(Year, Month, Day);
+                Person newPerson = new Person(gender, hair, eyeColor, Birthday);
+                PersonList.Add(newPerson);
+
+                Console.WriteLine("Person tillagd framgångsrikt!");
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine($"Fel vid tillägg av person: ");
+            }
+        }
+        public static void ListPerson()
+        {
+            if (PersonList.Count == 0)
+            {
+                Console.WriteLine("Inga personer tillagda ännu.");
+                return;
+            }
+            foreach (var p in PersonList)
+            {
+                Console.WriteLine(p.ToString());
+            }
+        }
+
+        public override string ToString()
+        {
+            return $"Född: {Birthday:yyyy-MM-dd}" +
+                   $"\nKön: {Gender}" +
+                   $"\nÖgon: {EyeColor}" +
+                   $"\nHår: {Hair.Color} & {Hair.Length}";
+        }
     }
 }
--- Evil Eavesdrop Enterprises/Evil Eavesdrop Enterprises/Program.cs
+++ Evil Eavesdrop Enterprises/Evil Eavesdrop Enterprises/Program.cs
@@ -2,58 +2,46 @@
 
 namespace Evil_Eavesdrop_Enterprises
 {
-    //Énum för att hantera kön
-    public enum Gender { Kvinna, Man, Övrigt }
-
-    //Struct för att hantera hår
-    public struct Hair
-    {
-        public string Color;
-        public string Length;
-    }
-
-    //Klass för att hantera personen
-    public class Person
-    {
-        //Egenskaper
-        public Gender Gender { get; set; }
-        public Hair Hair { get; set; }
-        public string EyeColor { get; set; }
-        public DateTime Birthday { get; set; }
-
-        //Konstruktor för att initiera alla egenskaper vid skapandet av objektet
-        public Person(Gender gender, Hair hair, string eyeColor, DateTime birthday)
-        {
-            Gender = gender;
-            Hair = hair;
-            EyeColor = eyeColor;
-            Birthday = birthday;
-        }
-
-        //ToString metod för att skriva ut personens information
-        public override string ToString()
-        {
-            return $"Född: {Birthday:yyyy-MM-dd}" +
-                   $"\nKön: {Gender}" +
-                   $"\nÖgon: {EyeColor}" +
-                   $"\nHår: {Hair.Color} & {Hair.Length}";
-        }
-    }
-
     internal class Program
     {
         static void Main(string[] args)
         {
-            //För att skapa håret till konstruktorn
-            Hair myHair = new Hair { Color = "Grönt", Length = "långt" };
+            //Bool för att hålla igång programmet
+            bool runProgram = true;
 
-            //Skapa en person med hjälp av konstruktorn
-            Person p1 = new Person(Gender.Man, myHair, "lila", new DateTime(1995, 1, 17));
+            //Huvudloop för programmet
+            while (runProgram)
+            {
+                Console.WriteLine("Välkommen till Evil Eavesdrop Enterprises");
+                Console.WriteLine();
+                Console.WriteLine("Nummer 1: Lägg till en ny person");
+                Console.WriteLine("Nummer 2: Visa lista på alla personer");
+                Console.WriteLine("Nummer 3: Välj nummer 1 eller 2 för att gå vidare och 3 för att avsluta");
+                Console.WriteLine("Nummer 4: För att avsluta");
 
-            //Skriv ut personens information
-            Console.WriteLine(p1);
+                string choice = Console.ReadLine();
+                Console.WriteLine();
 
-            Console.ReadLine();
+                switch (choice)
+                {
+                    case "1":
+                        Console.WriteLine();
+                        Person.AddPerson();
+                        Console.WriteLine();
+                        break;
+                    case "2":
+                        Person.ListPerson();
+                        Console.WriteLine();
+                        break;
+                    case "3":
+                        runProgram = false;
+                        break;
+                    default:
+                        Console.WriteLine("Bror? Det står väl nummer 1 eller 2.. Prova igen");
+                        break;
+                        Console.WriteLine();
+                }
+            }
         }
     }
 }
